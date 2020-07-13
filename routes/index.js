8f9ab9384129cb08caba855fc6443a2d3338b733
WB <esayas@wangbings-MacBook-Pro.local>
2020-07-13 11:53:07 +0800

    分页

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,37 @@ const { Db } = require('mongodb');
 var router = express.Router();
 // 引入model
 var model = require('../model');
+var moment = require('moment')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var username = req.session.username;
+  var username = req.session.username || '';
+  var page = req.query.page || 1;
+  var data = {
+    total: 0,  // 总共有多页
+    curPage: page,
+    list: []   // 当前页的文章列表
+  }
+  var pageSize = 2  //每次请求两条数据
   model.connect(function (db) {
+    // 1-第一步查询所有文章
     db.collection('articles').find().toArray(function (err, docs) {
-      console.log('article', docs)
-      res.render('index', { username: username });
+      data.total = Math.ceil(docs.length / pageSize)
+      // 2-查询当前页的文章列表
+      model.connect(function (db) {
+        // sort()  limit()  skip()
+        db.collection('articles').find().sort({ _id: -1 }).limit(pageSize).skip((page - 1) * pageSize).toArray(function (err, docs2) {
+          if (docs2.length == 0) {
+            res.redirect('/?page=' + ((page - 1) || 1))
+          } else {
+            docs2.map(function (ele, index) {
+              ele['time'] = moment(ele.id).format('YYYY-MM-DD HH:mm:ss')
+            })
+            data.list = docs2
+          }
+          res.render('index', { username: username, data: data });
+        })
+      })
     })
   })
 
